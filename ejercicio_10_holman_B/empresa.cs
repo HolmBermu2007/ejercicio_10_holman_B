00aae76a63aa7a14fb37f6354ca9d511a93c2af2
Admin <Admin@DESKTOP-VBJGGPH>
2023-05-05 10:52:51 +0200

    Agregar archivos de proyecto.

---
+++ ejercicio_10_holman_B/empresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_10_holman_B
+{
+    internal class empresa
+    {
+        private string nombre;
+
+        public empresa(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+}
